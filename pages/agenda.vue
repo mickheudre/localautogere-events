d335a83c1b8a5f5273fd30d1e4f61236b62b1afe
Mick <mickheudre@gmail.com>
2022-11-24 16:37:44 +0100

    ajout d'une catégorie evenements passés

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="max-h-screen overflow-y-auto">
-        <div class="mb-6" v-for="block in events.results" :key="block.id">
-            <h3 class="capitalize font-semibold"> {{ dateToString(block.properties.Date.date.start) }}</h3>
-            <span class="italic text-sm"> {{ formatTags(block.properties.Tags.multi_select) }} </span>
-            <h2 class="">
-                <span  v-for="title in block.properties.Name.title" :key="title.id">
-                    {{ title.plain_text }}
-                </span>
-            </h2>
-            <p class="">
-                <span  v-for="title in block.properties.Description.rich_text" :key="title.id">
-                    {{ title.plain_text }}
-                </span>
-            </p>
-            
-            <!-- <p v-for=" text in block.paragraph.text" :key="text.id">
-                <span v-if="text.text.link === null"> {{ text.text.content }}</span>
-                <span v-else> <a target="_parent" :href="text.text.link.url">{{ text.text.content }}</a></span>
-            </p> -->
+        <div class="mb-6" v-for="block in events.results"  :key="block.id">
+            <div v-if="block.properties.Incoming.formula.boolean"> 
+                <h3 class="capitalize font-semibold"> {{ dateToString(block.properties.Date.date.start) }}</h3>
+                <span class="italic text-sm"> {{ formatTags(block.properties.Tags.multi_select) }} </span>
+                <h2 class="">
+                    <span  v-for="title in block.properties.Name.title" :key="title.id">
+                        {{ title.plain_text }}
+                    </span>
+                </h2>
+                <p class="">
+                    <span  v-for="title in block.properties.Description.rich_text" :key="title.id">
+                        {{ title.plain_text }}
+                    </span>
+                </p>
+            </div>
+        </div>
+        <h3 class="text-3xl font-semibold capitalize mt-12">évènements passés</h3>
+        <div class="mb-6" v-for="block in events.results"  :key="block.id">
+            <div v-if="!block.properties.Incoming.formula.boolean"> 
+                <h3 class="capitalize font-semibold"> {{ dateToString(block.properties.Date.date.start) }}</h3>
+                <span class="italic text-sm"> {{ formatTags(block.properties.Tags.multi_select) }} </span>
+                <h2 class="">
+                    <span  v-for="title in block.properties.Name.title" :key="title.id">
+                        {{ title.plain_text }}
+                    </span>
+                </h2>
+                <p class="">
+                    <span  v-for="title in block.properties.Description.rich_text" :key="title.id">
+                        {{ title.plain_text }}
+                    </span>
+                </p>
+            </div>
         </div>
     </div>
 </template>
@@ -39,7 +53,7 @@ export default Vue.extend({
             }
             ]
         }
-
+        
         const events = await $axios.$post("https://api.notion.com/v1/databases/2e87451f6d604576ab1e5b37dea65590/query", data );
         return { events };
     },
@@ -50,7 +64,7 @@ export default Vue.extend({
         },
         formatTags(tags : Array<any>) {
             let tagsList = new Array<string>();
-
+            
             tags.forEach(element => {
                 tagsList.push(element.name)
             });
