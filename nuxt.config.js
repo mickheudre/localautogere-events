39e92ee66c79a5af73cb1cd7fcc5dcab0b7b078e
mick <mickheudre@gmail.com>
2025-11-26 18:39:11 +0100

    oops

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: [
     '@nuxtjs/tailwindcss',
-    ['./nuxt-notion/module', {output: ".vercel/output/static"}],
-
   ],
 })
